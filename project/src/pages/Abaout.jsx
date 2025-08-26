9a11bbddb98871a64d83a05f1c2a9ab91896f02e
rashid <rashdo5jr@gmail.come>
2025-08-26 07:39:00 +0300

    router

---
+++ project/src/pages/Abaout.jsx
@@ -0,0 +1,7 @@
+function About(){
+    return(
+      <h1>dfgy</h1>  
+    );
+}
+
+export default About;
---
+++ project/src/pages/Contact.jsx
@@ -0,0 +1,7 @@
+function Contact(){
+    return(
+      <h1>dfgy</h1>  
+    );
+}
+
+export default Contact;
