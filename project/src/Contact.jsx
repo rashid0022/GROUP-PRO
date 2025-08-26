8371b1d81da48c012863c9b1bf94b7c43fb55c51
rashid <rashdo5jr@gmail.come>
2025-08-26 10:08:04 +0300

    Normalize line endings

--- project/src/Contact.jsx
+++ project/src/Contact.jsx
@@ -1,7 +1,34 @@
-function Contact(){
-    return(
-      <h1>dfgy</h1>  
-    );
-}
+function contact() {
+  return(
+  <>
+  <div className="contact"> 
+    <h1>contact form</h1>
+    <form action="/submit" method="post">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" required />
+    
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required />
 
-export default Contact;
+        <label for="address">Adress:</label>
+        <input type="text" id="address" name="address" required />
+
+        <label for="phone">Phone Number:</label>
+        <input type="tel" id="phone" name="phone" required />
+
+        <label for="message">Message</label>
+        <textarea id="message" name="message" rows="6" required></textarea>
+
+        <button type="submit">Submit</button>
+    </form>
+    
+
+  
+  
+  
+    
+   </div>
+</>
+) ;
+}
+export default contact;
