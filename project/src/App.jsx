f6ffcee9cf3b36fe7e843475f537741b444155c5
rashid <rashdo5jr@gmail.come>
2025-08-27 10:02:32 +0300

    change all

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
-
 function App() {
   return (
     <Routes>
